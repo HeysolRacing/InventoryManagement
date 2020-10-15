33900d09d1671211ac24804aec53b63c8c3eeedd
Heysel Sagaon <HSagaon@elementcorp.com>
2020-10-14 19:41:05 -0500

    Add project files.

---
+++ InventoryManagement/Controllers/DepartmentController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Configuration;
+
+namespace InventoryManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly string sConnString = "Server=.;Database=InventoryManagement;Integrated Security=true";
+        private readonly ILogger<DepartmentController> _logger;
+
+        public DepartmentController(ILogger<DepartmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Department> Get()
+        {
+            string query = @"SELECT DepartmentId, DepartmentName FROM dbo.Department";
+            DataTable dt = new DataTable();
+
+            using (var con = new SqlConnection(sConnString))
+            {
+                using(var cmd = new SqlCommand(query, con))
+                {
+                    using (var da = new SqlDataAdapter(cmd))
+                    { 
+                        cmd.CommandType = CommandType.Text;
+                        da.Fill(dt);
+                    }
+                }
+            }
+
+            return dt.AsEnumerable().Select(row => new Department
+            { 
+                DepartmentId = Convert.ToInt32(row["DepartmentId"]),
+                DepartmentName = row["DepartmentName"].ToString()
+
+            });
+        }
+
+        [HttpPost]
+        public string Post(Department department)
+        {
+            try 
+            {
+                string query = @" INSERT INTO [Department] ([DepartmentName]) VALUES ('" + department.DepartmentName + "')";
+                
+                using (var con = new SqlConnection(sConnString))
+                {
+                    con.Open();
+                    using (var cmd = new SqlCommand(query, con))
+                    {
+                       cmd.CommandType = CommandType.Text;
+                       cmd.ExecuteNonQuery();
+                    }
+
+                    con.Close();
+                }
+            }
+            catch(Exception ex) 
+            {
+                string error = ex.Message;
+            }
+
+            return "Add successfully";
+        }
+
+        [HttpPut]
+        public string Put(Department department)
+        {
+            try
+            {
+                string query = @"UPDATE dbo.Department SET DepartmentName = '" + department.DepartmentName + "'" +
+                                "WHERE DepartmentId = " + department.DepartmentId;
+   
+                using (var con = new SqlConnection(sConnString))
+                {
+                    con.Open();
+                    using (var cmd = new SqlCommand(query, con))
+                    {
+                       cmd.CommandType = CommandType.Text;
+                       cmd.ExecuteNonQuery();
+                    }
+                   
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return "Update successfully";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"DELETE FROM dbo.Department WHERE DepartmentId = " + id.ToString();
+     
+                using (var con = new SqlConnection(sConnString))
+                {
+                    con.Open();
+                    using (var cmd = new SqlCommand(query, con))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return "Delete successfully";
+        }
+    }
+}
---
+++ InventoryManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace InventoryManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly string sConnString = "Server=.;Database=InventoryManagement;Integrated Security=true";
+    
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            DataTable dt = new DataTable();
+
+            string sConnString = "Server =.; Database = InventoryManagement; Integrated Security = true";
+
+            using (var con = new SqlConnection(sConnString))
+            {
+                using (var cmd = new SqlCommand("[dbo].[GetEmployee]", con))
+                {
+                    using (var da = new SqlDataAdapter(cmd))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        da.Fill(dt);
+                    }
+                }
+            }
+
+            return dt.AsEnumerable().Select(row => new Employee
+            {
+                EmployeeId = Convert.ToInt32(row["EmployeeID"]),
+                EmployeeName = row["EmployeeName"].ToString(),
+                Department = row["Department"].ToString(),
+                DateofHire = row["DateofHire"].ToString(),
+                PhotoFileName = row["PhotoFileName"].ToString(),
+
+            });
+        }
+
+        [HttpPost]
+        public string Post(Employee employee)
+        {
+            try
+            {
+                using (var con = new SqlConnection(sConnString))
+                {
+                    using (var cmd = new SqlCommand("[dbo].[AddEmployee]", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.Add("@EmployeeName", SqlDbType.VarChar).Value = employee.EmployeeName;
+                        cmd.Parameters.Add("@Department", SqlDbType.VarChar).Value = employee.Department;
+                        cmd.Parameters.Add("@HireDate", SqlDbType.Date).Value = employee.DateofHire;
+                        cmd.Parameters.Add("@PhotoFileName", SqlDbType.VarChar).Value = employee.PhotoFileName;
+                        
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return "Add successfully";
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public string SaveFile()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Request;
+                var postedFile = httpRequest;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                throw;
+            }
+
+            return "anonymous.png";
+        }
+    }
+}
